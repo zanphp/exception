936ed4a84f5c823c95299b4ea3470a750db3e52d
chuxiaofeng <chuxiaofeng@youzan.com>
2017-07-20 18:54:38 +0800

    Revert "Merge branch 'revert-3f997200' into 'master'"
    This reverts merge request !1

---
+++ src/BusinessException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ZanPHP\Exception;
+
+
+class BusinessException extends ZanException
+{
+    /**
+     * 验证是否为业务异常编码
+     * @param int $code
+     * @return bool
+     */
+    public static function isValidCode($code)
+    {
+        return ($code >= 10000 && $code <= 60000) || strlen($code) === 9;
+    }
+}
