3eb9626b34ee04bd3c4f8f7461fec632fff96a14
chuxiaofeng <chuxiaofeng@youzan.com>
2017-07-21 16:54:54 +0800

    rm condition exception

--- META-INF/alias.php
+++ META-INF/alias.php
@@ -4,7 +4,6 @@ return [
     \ZanPHP\Exception\ZanException::class       => "\\Zan\\Framework\\Foundation\\Exception\\ZanException",
     \ZanPHP\Exception\SystemException::class    => "\\Zan\\Framework\\Foundation\\Exception\\SystemException",
     \ZanPHP\Exception\BusinessException::class  => "\\Zan\\Framework\\Foundation\\Exception\\BusinessException",
-    \ZanPHP\Exception\ConditionException::class => "\\Zan\\Framework\\Foundation\\Exception\\ConditionException",
 
     \ZanPHP\Exception\System\ErrorException::class              => "\\Zan\\Framework\\Foundation\\Exception\\System\\ErrorException",
     \ZanPHP\Exception\System\ClassNotFoundException::class      => "\\Zan\\Framework\\Foundation\\Exception\\System\\ClassNotFoundException",
